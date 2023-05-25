2cee3b2f2b2bc88b4f2e73f4e237056c1621cef1
Shanon Holland <105896063+sm0526@users.noreply.github.com>
2023-05-24 22:53:46 -0600

    added in functionality to make the contact form send the info to a google sheet, also made it clear after submit

--- script/script.js
+++ script/script.js
@@ -1,6 +1,9 @@
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 var sidemenu = document.getElementById("sidemenu");
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwFl0OG7pptwP67UB4upTz7GvO55aghdVqQ6LmCeesW1n3UetDZ1HrYVUHy6c83B9JYgA/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
 
 function opentab(tabname){
     for(tablink of tablinks){
@@ -15,8 +18,21 @@ function opentab(tabname){
 
 function openmenu(){
     sidemenu.style.right = "0";
-}
+};
 
 function closemenu(){
     sidemenu.style.right = "-200px";
-}
+};
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => {
+            msg.innerHTML = "Message sent successfully"
+            setTimeout(function(){
+                msg.innerHTML = ""
+            },5000)
+            form.reset()
+        })
+        .catch(error => console.error('Error!', error.message))
+})
